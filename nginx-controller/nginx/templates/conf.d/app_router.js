c8b5b21a15d80229e07807256adf6f807ee94350
osboxes.org <antony.chackery@thomsonreuters.com>
2017-12-13 12:25:49 -0500

    added initial app routing functionality

---
+++ nginx-controller/nginx/templates/conf.d/app_router.js
@@ -0,0 +1,34 @@
+function findAppType(appName) {
+	var fs = require('fs');
+ 	var contents = fs.readFileSync('/TR/data/appmanifest/AppStoreManifest.json');
+    	var appManifest = JSON.parse(contents);
+	
+	
+	for(var i=0;i<appManifest.apps.length;i++) 
+		if(appManifest.apps[i].name.toUpperCase() == appName.toUpperCase())
+			return appManifest.apps[i].versions[0].defaultPlatform;
+		
+	return "";
+}
+
+function getPlatformType(req, res) {
+    var splitUrl = req.uri.split('/');
+    var appPrefixIndex = 0;
+    var appName = "";
+    for(var i=0;i<splitUrl.length;i++)
+    {
+       if(splitUrl[i].toUpperCase() == "APPS")
+   	{
+          appPrefixIndex = i;
+          break;
+	}
+    }
+
+    if(appPrefixIndex >= 0 && appPrefixIndex <= splitUrl.length - 1)
+    {
+    	appName = splitUrl[appPrefixIndex + 1];
+    }
+
+    return findAppType(appName);
+}
+
